cbf542196b9d4ad881ff19beb64009454e2b8b4a
John Evans <evanjo03@luther.edu>
2019-04-24 17:17:14 -0500

    Updates

---
+++ precache-manifest.bace1810ed6a1461638d49fa35443afc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cb98b2321690154c3750a6b1b752dfa",
+    "url": "/clickGame/index.html"
+  },
+  {
+    "revision": "52cef3d09f783d4667d2",
+    "url": "/clickGame/static/css/main.95569bc2.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clickGame/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "52cef3d09f783d4667d2",
+    "url": "/clickGame/static/js/main.3ac195a9.chunk.js"
+  },
+  {
+    "revision": "2cccf4db22d9fda060a7",
+    "url": "/clickGame/static/js/runtime~main.7080151e.js"
+  },
+  {
+    "revision": "c27a915510f8144a138dddb82d04a23a",
+    "url": "/clickGame/static/media/background.c27a9155.jpg"
+  }
+]);
