3c9b56e71225056c67d853c86a75b1b477b06a5c
John Evans <evanjo03@luther.edu>
2019-04-24 17:11:18 -0500

    just need to deploy

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,20 +4,19 @@ import "./style.css"
 const style =  {
     width: "100%",
     textAlign: "center",
-    fontSize: "3rem",
     color: "#black",
     marginBottom: "50px",
     padding: "2rem",
     borderBottom: "1px solid white",
     borderTop: "1px solid white"
-
   }
 
+  
 function Header(props) {
     return (
         <div className="jumbotron-fluid" style={style}>
             <h1 className="display-4">Avengers Memory Game</h1>
-            <p className="lead">Try and click as many cards as you can, but don't choose the same card twice!</p>
+            <p>Try and click as many cards as you can, but don't choose the same card twice!</p>
             <span>{props.display}</span>
             <hr></hr>
             <span className="score" >Score: {props.score}   High Score: {props.highscore}</span>
